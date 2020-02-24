40e4bd1eba6a1cf36bf243bec252f5eaa75a0654
Kholkin <kholkins@mail.ru>
2020-02-24 20:15:27 +0300

    set element

--- app/src/main/java/com/example/andro2_2/MainActivity.java
+++ app/src/main/java/com/example/andro2_2/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btnCreate;
     Button btnClear;
 
+    int wrapContent = LinearLayout.LayoutParams.WRAP_CONTENT;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +54,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // для tvColor и tvSize необходимо создавать контекстное меню
         registerForContextMenu(tvColor);
         registerForContextMenu(tvSize);
+
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        rgGravity = (RadioGroup) findViewById(R.id.rgGravity);
+        etName = (EditText) findViewById(R.id.etName);
+
+        btnCreate = (Button) findViewById(R.id.btnCreate);
+        btnCreate.setOnClickListener(this);
+
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClear.setOnClickListener(this);
     }
 
     @Override
@@ -140,6 +153,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnCreate:
+                // Создание LayoutParams c шириной и высотой по содержимому
+                LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
+                        wrapContent, wrapContent);
+                // переменная для хранения значения выравнивания
+                // по умолчанию пусть будет LEFT
+                int btnGravity = Gravity.LEFT;
+                // определяем, какой RadioButton "чекнут" и
+                // соответственно заполняем btnGravity
+                switch (rgGravity.getCheckedRadioButtonId()) {
+                    case R.id.rbLeft:
+                        btnGravity = Gravity.LEFT;
+                        break;
+                    case R.id.rbCenter:
+                        btnGravity = Gravity.CENTER_HORIZONTAL;
+                        break;
+                    case R.id.rbRight:
+                        btnGravity = Gravity.RIGHT;
+                        break;
+                }
+                // переносим полученное значение выравнивания в LayoutParams
+                lParams.gravity = btnGravity;
 
+                // создаем Button, пишем текст и добавляем в LinearLayout
+                Button btnNew = new Button(this);
+                btnNew.setText(etName.getText().toString());
+                llMain.addView(btnNew, lParams);
+
+                break;
+            case R.id.btnClear:
+                llMain.removeAllViews();
+                Toast.makeText(this, "Удалено", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
