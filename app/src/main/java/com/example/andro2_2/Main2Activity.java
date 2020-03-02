9f5a95eaf40707fdc7e50b05aa69998c9c8c15ad
Kholkin <kholkins@mail.ru>
2020-03-02 15:22:24 +0300

    Notification

--- app/src/main/java/com/example/andro2_2/Main2Activity.java
+++ app/src/main/java/com/example/andro2_2/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.andro2_2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -132,6 +134,10 @@ public class Main2Activity extends AppCompatActivity implements SeekBar.OnSeekBa
     }
 
     public void showNotification (View view){
-
+        Notification.Builder builder = new Notification.Builder(getApplicationContext());
+        Intent intent = new Intent(getApplicationContext(), FinishActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+        builder
+                .setContentIntent(pendingIntent);
     }
 }
