f0dd9e95d0d5789f61c4eca114673b63819a96f4
Kholkin <kholkins@mail.ru>
2020-03-07 21:43:16 +0300

    Notification

--- app/src/main/java/com/example/andro2_2/ButtonFragment.java
+++ app/src/main/java/com/example/andro2_2/ButtonFragment.java
@@ -1,6 +1,19 @@
 package com.example.andro2_2;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ButtonFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.button_layout, container, false);
+    }
 }
